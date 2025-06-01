c3e92ff3cc93afd06954e094239a9e8687aa9be3
Jose Isaac Blanco Sandoval <4673800+SValac@users.noreply.github.com>
2025-05-31 19:00:42 -0600

    Tl 11 (#51)
    * setup tailwind and daisy ui and icons
    
    * feat: restructure app layout and add navigation bar
    
    * fix: add newline at end of app.vue file

--- app/app.vue
+++ app/app.vue
@@ -1,10 +1,7 @@
 <template>
   <div>
-    <h1 class="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-    <button class="btn btn-info">
-      view Map<Icon name="tabler:map" />
-    </button>
+    <NuxtLayout>
+      <NuxtPage />
+    </NuxtLayout>
   </div>
 </template>
---
+++ app/components/app/nav-bar.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <nav class="navbar bg-primary text-primary-content">
+    <div class="navbar-start">
+      <NuxtLink to="/" class="btn btn-ghost text-xl">
+        Travel Log
+      </NuxtLink>
+    </div>
+    <div class="navbar-end">
+      <a class="btn btn-accent">Sign in
+        <Icon name="tabler:brand-github" size="24" />
+      </a>
+    </div>
+  </nav>
+</template>
+
+<style scoped>
+
+</style>
