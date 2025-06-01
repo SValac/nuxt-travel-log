579329cb466528686e848b29ecd911793df0eaf3
Jose Isaac Blanco Sandoval <4673800+SValac@users.noreply.github.com>
2025-05-31 18:55:17 -0600

    feat: restructure app layout and add navigation bar

---
+++ app/layouts/default.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div class="flex min-h-dvh flex-col">
+    <AppNavBar />
+    <main>
+      <slot />
+    </main>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
