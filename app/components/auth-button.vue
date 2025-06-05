87df428a6425d3d670746bffb2265c189dd90ab6
Jose Isaac Blanco Sandoval <4673800+SValac@users.noreply.github.com>
2025-06-05 17:52:17 -0600

    setup auth using github

---
+++ app/components/auth-button.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import { createAuthClient } from 'better-auth/client';
+
+const authClient = createAuthClient();
+
+async function signIn() {
+  await authClient.signIn.social({
+    provider: 'github',
+    callbackURL: '/dashboard',
+  });
+
+  // do not need the return value because it will redirect the user and handle the authentication flow
+}
+</script>
+
+<template>
+  <button class="btn btn-accent" @click="signIn">
+    Sign in
+    <Icon name="tabler:brand-github" size="24" />
+  </button>
+</template>
+
+<style scoped>
+
+</style>
