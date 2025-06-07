2af19b0388373b248318a4cbe30438748b998282
Jose Isaac Blanco Sandoval <4673800+SValac@users.noreply.github.com>
2025-06-06 23:32:30 -0600

    added log out functionality

---
+++ app/pages/sign-out.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+const authStore = useAuthStore();
+
+onMounted(() => {
+  authStore.signOut();
+});
+</script>
+
+<template>
+  <div class="card bg-base-300 container mx-auto mt-10 min-h-72, text-center flex flex-col items-center justify-center gap-4">
+    <span
+      class="loading loading-spinner loading-xl"
+    />
+  </div>
+</template>
+
+<style scoped>
+
+</style>
