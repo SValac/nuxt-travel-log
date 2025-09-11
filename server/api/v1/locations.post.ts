c4fc7a9c1169afc55933bb48d3fcb5c817494db0
Jose Isaac Blanco Sandoval <4673800+SValac@users.noreply.github.com>
2025-09-11 11:42:43 -0600

    Tl 24 (#63)
    * feat: enhance error handling for location insertion and ensure unique slug constraint
    
    * feat: add slug package and its TypeScript definitions
    
    * feat: replace slug generation with slug package for improved consistency
    
    * feat: add nanoid package for unique ID generation
    
    * feat: update slug generation to ensure uniqueness with nanoid fallback
    
    * feat: refactor slug uniqueness check to improve clarity and maintainability
    
    * feat: refactor slug generation to use a pre-defined nanoId function for improved clarity
    
    * feat: add initial database migration for account, session, user, and location tables with unique constraints

--- server/api/v1/locations.post.ts
+++ server/api/v1/locations.post.ts
@@ -1,5 +1,13 @@
+import type { LibsqlError } from '@libsql/client';
+import type { DrizzleError } from 'drizzle-orm';
+
 import db from '~~/lib/db';
 import { location, locationInsertSchema } from '~~/lib/db/schema';
+import { and, eq } from 'drizzle-orm';
+import { customAlphabet } from 'nanoid';
+import slugify from 'slug';
+
+const nanoId = customAlphabet('abcdefghijklmnopqrstuvwxyz0123456789', 5);
 
 export default defineEventHandler(async (event) => {
   if (!event.context.user) {
@@ -34,11 +42,69 @@ export default defineEventHandler(async (event) => {
     }));
   }
 
-  const [created] = await db.insert(location).values({
-    ...response.data,
-    slug: response.data.name.toLowerCase().replace(/\s+/g, '-'),
-    userId: event.context.user?.id,
-  }).returning();
+  const existingLocation = await db.query.location.findFirst({
+    where: and(
+      eq(location.name, response.data.name),
+      eq(location.userId, event.context.user.id),
 
-  return created;
+    ),
+  });
+
+  if (existingLocation) {
+    return sendError(event, createError({
+      statusCode: 409,
+      statusMessage: 'Location with this name already exists',
+    }));
+  }
+
+  let slug = slugify(response.data.name);
+  let existingSlug = !!(await db.query.location.findFirst({
+    where: eq(location.slug, slug),
+  }));
+
+  /*
+    TODO: This while loop is a temporary solution to ensure slug uniqueness.
+    In a high-concurrency environment (like a web application with many users), this could lead to race conditions.
+    A more robust solution would involve database-level constraints or
+    a different strategy for generating unique slugs.
+
+    Before implementing a more robust solution, this approach should suffice for most use cases. But be aware of its limitations.
+    Additionally, consider implementing a retry mechanism or a more sophisticated slug generation strategy if you expect high traffic.
+
+    The loop checks if the generated slug already exists in the database. If it does, it appends a random string to the slug
+    using nanoid until a unique slug is found.
+    This ensures that even if two locations have the same name, they will have different slugs.
+  */
+  while (existingSlug) {
+    const id = nanoId();
+    const slugId = `${slug}-${id}`;
+    existingSlug = !!(await db.query.location.findFirst({
+      where: eq(location.slug, slugId),
+    }));
+    if (!existingSlug) {
+      slug = slugId;
+      break;
+    }
+  }
+
+  try {
+    const [created] = await db.insert(location).values({
+      ...response.data,
+      slug,
+      userId: event.context.user?.id,
+    }).returning();
+
+    return created;
+  }
+  catch (e) {
+    const drizzleError = e as DrizzleError;
+    const errorMessage = drizzleError.cause as LibsqlError;
+    if (errorMessage.message === 'SQLITE_CONSTRAINT: SQLite error: UNIQUE constraint failed: location.slug') {
+      return sendError(event, createError({
+        statusCode: 409,
+        statusMessage: 'Slug must be unique (the location name is used to generate the slug)',
+      }));
+    }
+    throw drizzleError;
+  }
 });
