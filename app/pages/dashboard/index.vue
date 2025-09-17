eb4c4d18e967947e41da1ea9d18bf901b74410f0
Jose Isaac Blanco Sandoval <4673800+SValac@users.noreply.github.com>
2025-09-17 03:06:51 -0600

    feat: add dashboard page with location fetching and user interaction handling

---
+++ app/pages/dashboard/index.vue
@@ -0,0 +1,59 @@
+<script setup lang="ts">
+/*
+  we dont use $fetch because this is used in response to user events/actions (clicking, typing, etc)
+  if we want the data to be available when page load and you know that request can happen at the top level
+  before user interaction, use useFetch instead.
+
+  useFetch hanldles loading state, errors, and is SSR friendly. its loads data on server side during SSR
+  and then hydrates it on client side. and includes all headers and make that request in the context of the user
+  session (cookies, auth tokens, etc)
+
+*/
+
+const { data, status } = useFetch('/api/v1/locations', {
+  // allow navigating away from the page while the request is in flight
+  // without cancelling the request
+  lazy: true,
+  // initial cache time is 0, so its always fresh
+  // we can set a stale time if we want to revalidate after a certain period
+  // initialCache: 0,
+  // staleTime: 1000 * 60, // 1 minute
+  // refetch on window focus
+  // refetchOnWindowFocus: true,
+  // refetch on reconnect
+  // refetchOnReconnect: true,
+  // refetch when component is mounted
+  // refetchOnMount: true,
+});
+</script>
+
+<template>
+  <div class="p-4">
+    <h2 class="text-2xl font-bold mb-4">
+      Locations
+    </h2>
+    <div v-if="status === 'pending'">
+      <span class="loading loading-spinner loading-xl" />
+    </div>
+    <div v-else-if="data">
+      <pre>{{ JSON.stringify(data, null, 2) }}</pre>
+    </div>
+    <div
+      v-else
+      class="flex flex-col gap-2 mt-4"
+    >
+      <p>Add a location to get started</p>
+      <NuxtLink
+        to="/dashboard/add"
+        class="btn btn-primary mt-4 w-40"
+      >
+        <Icon name="tabler:tabler:circle-plus-filled" size="24" />
+        Add Location
+      </NuxtLink>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
