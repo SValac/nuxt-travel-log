1dcd13fd24763748e6e638da45c8c2bed893a3f8
Jose Isaac Blanco Sandoval <4673800+SValac@users.noreply.github.com>
2025-10-09 03:23:42 -0600

    refactor: restructure search API handler for improved readability , chance locations and error handling

--- server/api/v1/search.get.ts
+++ server/api/v1/search.get.ts
@@ -5,34 +5,43 @@ const searchSchema = z.object({
   q: string().min(1),
 });
 
-export default defineAuthenticatedEventHandler(async (event) => {
-  const result = await getValidatedQuery(event, searchSchema.safeParse);
+export default defineAuthenticatedEventHandler(
+  defineCachedEventHandler(async (event) => {
+    const result = await getValidatedQuery(event, searchSchema.safeParse);
 
-  if (!result.success) {
-    return sendZodError(event, result.error);
-  }
+    if (!result.success) {
+      return sendZodError(event, result.error);
+    }
 
-  try {
-    const response = await fetch(`https://nominatim.openstreetmap.org/search?q=${result.data.q}&format=json`, {
-      signal: AbortSignal.timeout(5000),
-      headers: {
-        'User-Agent': 'valac-nuxt-travel-log | shiro.valac@gmail.com',
-      },
-    });
+    try {
+      const response = await fetch(`https://nominatim.openstreetmap.org/search?q=${result.data.q}&format=json`, {
+        signal: AbortSignal.timeout(5000),
+        headers: {
+          'User-Agent': 'valac-nuxt-travel-log | shiro.valac@gmail.com',
+        },
+      });
 
-    if (!response.ok) {
+      if (!response.ok) {
+        return sendError(event, createError({
+          statusCode: 504,
+          statusMessage: 'unable to reach search api',
+        }));
+      }
+      const results = await response.json();
+      return results;
+    }
+    catch {
       return sendError(event, createError({
         statusCode: 504,
         statusMessage: 'unable to reach search api',
       }));
     }
-    const results = await response.json();
-    return results;
-  }
-  catch {
-    return sendError(event, createError({
-      statusCode: 504,
-      statusMessage: 'unable to reach search api',
-    }));
-  }
-});
+  }, {
+    maxAge: 60 * 60 * 24, // 24 hours
+    name: 'search-nominatim',
+    getKey: async (event) => {
+      const query = await getQuery(event);
+      return query.q?.toString() || '';
+    },
+  }),
+);
