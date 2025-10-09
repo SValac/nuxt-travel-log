398ae25a584b4f455046bcc3e27045e44d58648f
Jose Isaac Blanco Sandoval <4673800+SValac@users.noreply.github.com>
2025-10-09 02:43:52 -0600

    refactor: simplify error handling by introducing sendZodError utility

--- server/api/v1/locations.post.ts
+++ server/api/v1/locations.post.ts
@@ -4,32 +4,14 @@ import type { DrizzleError } from 'drizzle-orm';
 import { findLocationByName, findUniqueSlug, insertLocation } from '~~/lib/db/queries/location';
 import { locationInsertSchema } from '~~/lib/db/schema';
 import defineAuthenticatedEventHandler from '~~/server/utils/define-authenticated-event-handler';
+import sendZodError from '~~/server/utils/send-zod-error';
 import slugify from 'slug';
 
 export default defineAuthenticatedEventHandler(async (event) => {
   const response = await readValidatedBody(event, locationInsertSchema.safeParse);
 
   if (!response.success) {
-    const statusMessage = response
-      .error
-      .issues
-      .map(issue => `${issue.path.join('')}: ${issue.message}`)
-      .join(';');
-
-    const data = response
-      .error
-      .issues
-      .reduce((errors, issue) => {
-        errors[issue.path.join('')] = issue.message;
-
-        return errors;
-      }, {} as Record<string, string>);
-
-    return sendError(event, createError({
-      statusCode: 422,
-      statusMessage,
-      data,
-    }));
+    return sendZodError(event, response.error);
   }
 
   const existingLocation = await findLocationByName(response.data, event.context.user.id);
