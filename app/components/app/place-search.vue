dc174e2b882aba853dd20d6a1212908678e55b78
Jose Isaac Blanco Sandoval <4673800+SValac@users.noreply.github.com>
2025-10-10 01:10:52 -0600

    refactor: enhance search form structure and maintain search results state

--- app/components/app/place-search.vue
+++ app/components/app/place-search.vue
@@ -1,41 +1,73 @@
 <script setup lang="ts">
+import type { NominatimResult } from '~~/lib/types';
+
 import { SearchSchema } from '~~/lib/zod-schemas';
 
-function onSubmit(values: unknown) {
-  const { q } = values as SearchSchema;
-  console.log(q);
+const searchResults = ref<NominatimResult[]>([]);
+
+async function onSubmit(query: Record<string, string>) {
+  try {
+    const results = await $fetch('/api/v1/search', {
+      query,
+    });
+    searchResults.value = results;
+  }
+  catch (error) {
+    console.error(error);
+  }
 }
 </script>
 
 <template>
-  <Form
-    v-slot="{ errors }"
-    class="flex flex-col gap-2 items-center"
-    :validation-schema="toTypedSchema(SearchSchema)"
-    :initial-values="{ q: '' }"
-    @submit="onSubmit"
-  >
-    <div class="join mt-4">
-      <div>
-        <label class="input join-item">
-          <Icon name="tabler:search" />
-          <Field
-            type="text"
-            name="q"
-            placeholder="Search for a location"
-            :class="{
-              'input-error': errors.q,
-            }"
-          /></label>
-        <div v-if="true" class="validator-hint text-error visible">
-          {{ errors.q }}
+  <div class="flex flex-col gap-2">
+    <Form
+      v-slot="{ errors }"
+      class="flex flex-col gap-2 items-center"
+      :validation-schema="toTypedSchema(SearchSchema)"
+      :initial-values="{ q: '' }"
+      @submit="onSubmit"
+    >
+      <div class="join mt-4">
+        <div>
+          <label class="input join-item">
+            <Icon name="tabler:search" />
+            <Field
+              type="text"
+              name="q"
+              placeholder="Search for a location"
+              :class="{
+                'input-error': errors.q,
+              }"
+            /></label>
+          <div v-if="true" class="validator-hint text-error visible">
+            {{ errors.q }}
+          </div>
+        </div>
+        <button class="btn btn-neutral join-item">
+          Search
+        </button>
+      </div>
+    </Form>
+    <div class="flex flex-col gap-2 overflow-auto max-h-52">
+      <div
+        v-for="result in searchResults"
+        :key="result.place_id"
+        class="card card-xs bg-base-100"
+      >
+        <div class="card-body">
+          <h4 class="card-title">
+            {{ result.display_name }}
+          </h4>
+          <div class="justify-end card-actions">
+            <button class="btn btn-primary btn-sm">
+              Set Location
+              <Icon name="tabler:map-pin-share" size="18" />
+            </button>
+          </div>
         </div>
       </div>
-      <button class="btn btn-neutral join-item">
-        Search
-      </button>
     </div>
-  </Form>
+  </div>
 </template>
 
 <style scoped>
