9dc7af0388766c86cce27a487922f4917232ee36
Jose Isaac Blanco Sandoval <4673800+SValac@users.noreply.github.com>
2025-10-13 01:45:11 -0600

    feat: implement location details page with loading and error handling

--- app/pages/dashboard/location/[slug].vue
+++ app/pages/dashboard/location/[slug].vue
@@ -1,10 +1,33 @@
 <script setup lang="ts">
+const route = useRoute();
+const { slug } = route.params;
 
+// we use useFetch becase we want to call our own API endpoint
+const { data: location, status, error } = useFetch(`/api/v1/locations/${slug}`, {
+  lazy: true,
+});
 </script>
 
 <template>
-  <div>
-    Location Details Page
+  <div class="w-1/2 max-w-1/2 p-4 mx-auto">
+    <div v-if="status === 'pending'" class="text-center">
+      <div class="loading" />
+    </div>
+    <div v-if="location && status !== 'pending'">
+      <h2
+        class="text-xl font-bold mb-4 text-center"
+      >
+        {{ location?.name }}
+      </h2>
+      <p class="text-sm text-wrap">
+        {{ location?.description }}
+      </p>
+    </div>
+    <div v-if="error && status !== 'pending'" class="alert alert-error">
+      <h3>
+        {{ error.statusMessage }}
+      </h3>
+    </div>
   </div>
 </template>
 
